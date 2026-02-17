feed0648c3046358a329b71480d473c740665c35
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-02-17 15:47:21 -0500

    Edot zero-step check in parsePackage and implement DayActionInfo functions

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,10 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
 	"time"
 	"strings"
 	"strconv"
 	"errors"
+	"github.com/Yandex-Practicum/tracker/internal"
 )
 
 const (
@@ -18,17 +20,15 @@ func parsePackage(data string) (int, time.Duration, error) {
 	// TODO: реализовать функцию
 	parts := strings.Split(data, ",") // parsed input string
 	if len(parts) != 2 { // check len parts
-		err := errors.New("[ERROR] Len parts parsed from data less 2")
-		return 0, 0, err
+		return 0, 0, errors.New("len parts parsed from data less 2")
 	}
 
 	step, err := strconv.Atoi(parts[0]) // convert step in int
 	if err != nil {
 		return 0, 0, err
 	}
-	if step == 0 { // check quantity step
-		err := errors.New("[ERROR] Quantity step 0")
-		return 0, 0, err
+	if step <= 0 { // check quantity step
+		return 0, 0, errors.New("quantity step less 0")
 	}
 
 	t, err := time.ParseDuration(parts[1]) // parse time duration input in type time.Duration
@@ -41,4 +41,18 @@ func parsePackage(data string) (int, time.Duration, error) {
 
 func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
+	step, timeDuration, err := parsePackage(data) // get result call function parsePackage with param data
+	if err != nil {
+		fmt.Println(err) // output error in terminal
+		return ""
+	}
+	if step <= 0 { // check quantity step
+		return ""
+	}
+
+	durationStep := float64(step) * stepLength
+	distance := durationStep / mInKm
+	calories := internal.WalkingSpentCalories()
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", step, durationStep, calories)
 }
