bde42dd63df9f7b08245b386314d90098eef9851
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-02-17 14:47:36 -0500

    Implement parsePackage function

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,9 @@ package daysteps
 
 import (
 	"time"
+	"strings"
+	"strconv"
+	"errors"
 )
 
 const (
@@ -13,6 +16,23 @@ const (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	// TODO: реализовать функцию
+	parts := strings.Split(data, ",") // parsed input string
+	if len(parts) != 2 { // check len parts
+		err := errors.New("[ERROR] Len parts parsed from data less 2")
+		return 0, 0, err
+	}
+
+	step, err := strconv.Atoi(parts[0]) // convert step in int
+	if err != nil {
+		return 0, 0, err
+	}
+
+	t, err := time.ParseDuration(parts[1]) // parse time duration input in type time.Duration
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return step, t, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
